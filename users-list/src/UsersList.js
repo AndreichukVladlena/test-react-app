dcda3240fcece81f62e0e85e116088214de83f80
vlanuka <andreychuk04@mail.ru>
2024-10-20 21:16:21 +0300

    feat: add search filter

--- users-list/src/UsersList.js
+++ users-list/src/UsersList.js
@@ -2,18 +2,32 @@ import React, { useState, useEffect } from "react";
 
 const UsersList = () => {
   const [users, setUsers] = useState([]);
-  const [searchFilter, setSearchFilter] = useState([]);
+  const [searchFilter, setSearchFilter] = useState("");
 
   useEffect(() => {
     fetch("/users.json")
       .then((response) => response.json())
       .then((data) => setUsers(data));
   }, []);
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(searchFilter.toLowerCase())
+  );
+
   return (
     <>
       <h1>Users list</h1>
+      <input
+        type="text"
+        placeholder="search by users name..."
+        value={searchFilter}
+        onChange={(e) => {
+          setSearchFilter(e.target.value);
+        }}
+      />
+
       <ul>
-        {users.map((user) => (
+        {filteredUsers.map((user) => (
           <li key={user.id}>
             <h2>Name: {user.name}</h2>
             <p>Age: {user.age} years old</p>
