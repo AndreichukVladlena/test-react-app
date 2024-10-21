aa758dd4135a29f4749707e2aaf94cfef00c1a35
vlanuka <andreychuk04@mail.ru>
2024-10-21 22:27:55 +0300

    fix: entry files done

--- users-list/src/entry.js
+++ users-list/src/entry.js
@@ -1,1 +1,7 @@
-import("./index.js");
+import React from "react";
+import ReactDOM from "react-dom/client";
+import UsersList from "./UsersList";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<UsersList />);
--- weather-info/src/entry.js
+++ weather-info/src/entry.js
@@ -1,1 +1,6 @@
-import("./index.js");
+import React from "react";
+import ReactDOM from "react-dom";
+import WeatherInfo from "./WeatherInfo";
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(<WeatherInfo />);
