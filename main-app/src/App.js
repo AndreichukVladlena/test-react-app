b8b1744cf8edd8566edc7d04f15c41fbde28e274
vlanuka <andreychuk04@mail.ru>
2024-10-21 22:01:56 +0300

    feat: add menu to switch pages

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,10 +1,35 @@
-import React from "react"; // Must be imported for webpack to work
+import { lazy, Suspense } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+const UsersList = lazy(() => import("UsersList/UsersList"));
+const WeatherInfo = lazy(() => import("WeatherInfo/WeatherInfo"));
+
 function App() {
+  const [menu, setMenu] = useState("");
   return (
     <div className="App">
-      <div className="container">This is my home page</div>
+      <div>
+        <input
+          type="radio"
+          id="users"
+          value="users"
+          onClick={(e) => setMenu(e.target.value)}
+        />
+        <label htmlFor="users">Users List</label>
+        <input
+          type="radio"
+          id="weather"
+          value="weather"
+          onClick={(e) => setMenu(e.target.value)}
+        />
+        <label htmlFor="weather">Weather Info</label>
+      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        {!menu && <div>Choose page</div>}
+        {menu === "users" && <UsersList />}
+        {menu === "weather" && <WeatherInfo />}
+      </Suspense>
     </div>
   );
 }
